169aae31162bca2729a211824ea22c4268793c09
zetxx <zetxx@belogradchik.biz>
2023-11-22 21:31:15 +0200

    .

--- rotary.py
+++ rotary.py
@@ -1,13 +1,14 @@
 # https://github.com/miketeachman/micropython-rotary
+# https://www.keebtalk.com/t/how-to-wire-a-rotary-encoder-fit-in-a-matrix/6330
 
 import time
 from lib.rotary_irq_esp import RotaryIRQ
 
-r = RotaryIRQ(pin_num_clk=12, 
-              pin_num_dt=13, 
-              min_val=0, 
-              max_val=5, 
-              reverse=False, 
+r = RotaryIRQ(pin_num_clk=12,
+              pin_num_dt=13,
+              min_val=0,
+              max_val=5,
+              reverse=True,
               range_mode=RotaryIRQ.RANGE_UNBOUNDED)
               
 val_old = r.value()
