3f7cbe4eaae727b8a7247235bb8cc9d4cd84dd77
zetxx <zetxx@belogradchik.biz>
2023-11-23 20:48:12 +0200

    rotary

--- rotary.py
+++ rotary.py
@@ -8,25 +8,10 @@ from lib.button import Button
 
 r = RotaryIRQ(pin_num_clk=13,
               pin_num_dt=14,
-              min_val=0,
-              max_val=5,
-              reverse=True,
-              range_mode=RotaryIRQ.RANGE_UNBOUNDED)
-def rotaryPrint(v):
-    print('rotary:')
+              reverse=True)
+def rotaryPrint():
+    print('rotary')
+    print(r.value())
 
 r.add_listener(rotaryPrint)
 Button(pinnum=15, callback=lambda p:print(time.time()))
-
-# val_old = r.value()
-# while True:
-#     val_new = r.value()
-    
-#     if val_old != val_new:
-#         val_old = val_new
-#         print('result =', val_new)
-        
-#     time.sleep_ms(50)
-
-
-
